590f103f04f61002566f52652ea123eae6e07e70
moogmodular <michail.karassew@openforce.com>
2022-07-02 22:30:34 +0200

    fix: add port to lnurl service

--- src/server/services/lightning.ts
+++ src/server/services/lightning.ts
@@ -18,6 +18,7 @@ export const lnUrlService = require('lnurl').createServer({
             client: 'postgres',
             connection: {
                 host: process.env.DB_HOST,
+                port: process.env.DB_PORT,
                 user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
