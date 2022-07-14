365aee3dc75c2240cc8fc6749beb7c0af6830b37
moogmodular <michail.karassew@openforce.com>
2022-07-14 22:29:10 +0200

    chore: prettier

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/post/single/:slug',
-        destination: '/',
-      },
-      {
-        source: '/post/tag/:tag',
-        destination: '/',
-      },
-      {
-        source: '/post/user/:user',
-        destination: '/',
-      },
-      {
-        source: '/post/timeline',
-        destination: '/',
-      },
-    ]
-  },
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/post/single/:slug',
+                destination: '/',
+            },
+            {
+                source: '/post/tag/:tag',
+                destination: '/',
+            },
+            {
+                source: '/post/user/:user',
+                destination: '/',
+            },
+            {
+                source: '/post/timeline',
+                destination: '/',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
