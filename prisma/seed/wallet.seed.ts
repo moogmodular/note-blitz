26998a2f090fc783f2267208cf2607da3705fac7
moogmodular <michail.karassew@openforce.com>
2022-07-14 22:48:05 +0200

    fix: wallet seed file

---
+++ prisma/seed/wallet.seed.ts
@@ -0,0 +1,45 @@
+export const walletSeed = [
+    {
+        id: 1,
+        displayName: 'Alby',
+        description:
+            'Alby brings Bitcoin payments to the web with in-browser payments and identity, all with your own wallet.',
+        url: 'https://getalby.com/',
+        icon: 'https://getalby.com/website/_assets/banner-illustration-J7OVHJ7B.webp',
+        rank: 1,
+    },
+    {
+        id: 2,
+        displayName: 'Blixt Wallet',
+        description:
+            "Blixt Wallet is a non-custodial open-source Bitcoin Lightning Wallet for Android and iOS with a focus on usability and user experience. It's currently aimed towards Bitcoiners who want to try out using Lightning Network.",
+        url: 'https://blixtwallet.github.io/',
+        icon: 'https://blixtwallet.github.io/assets/images/blixt-wallet-icon.png',
+        rank: 2,
+    },
+    {
+        id: 3,
+        displayName: 'Breez',
+        description:
+            'Breez is the simplest, fastest and safest way to spend your bitcoins. Breez aims to drive bitcoin adoption in everyday commerce by providing a seamless Bitcoin usage. All powered by Lightning Network.',
+        url: 'https://breez.technology/',
+        icon: ' https://breez.technology/favicon.ico',
+        rank: 3,
+    },
+    {
+        id: 4,
+        displayName: 'muun',
+        description: 'Muun is a self-custodial wallet for bitcoin and lightning.',
+        url: 'https://muun.com/',
+        icon: 'https://muun.com/static/device-FooterBanner-57b68dc842a7bf761c575725d52ed731.png',
+        rank: 4,
+    },
+    {
+        id: 5,
+        displayName: 'Phoenix',
+        description: 'The Bitcoin wallet from the future',
+        url: 'https://phoenix.acinq.co/',
+        icon: 'https://phoenix.acinq.co/favicon.ico',
+        rank: 5,
+    },
+]
