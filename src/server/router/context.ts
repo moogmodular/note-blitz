bb4751741aa6a8f6840aec58c2093e735d415625
moogmodular <michail.karassew@openforce.com>
2022-07-20 18:58:25 +0200

    refactor: minor clean up

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,4 +1,3 @@
-// src/server/router/context.ts
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { unstable_getServerSession as getServerSession } from 'next-auth'
