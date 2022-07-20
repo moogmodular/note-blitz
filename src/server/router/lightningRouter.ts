76630ab783944b4c57b06f36cc5935100879bd05
moogmodular <michail.karassew@openforce.com>
2022-07-20 18:58:07 +0200

    feat: test node connection

--- src/server/router/lightningRouter.ts
+++ src/server/router/lightningRouter.ts
@@ -1,17 +1,20 @@
-import { getChainBalance, getInvoices, getPeers } from 'lightning'
+import { getChainBalance, getPeers, getWalletInfo } from 'lightning'
 
 import { createRouter } from './context'
 
-export const lightningRouter = createRouter().query('getNodeBalance', {
+export const lightningRouter = createRouter().mutation('nodeBalance', {
     async resolve({ ctx }) {
-        console.log('NODE BALANCE')
         const lnd = ctx.lnd
         const chainBalance = await getChainBalance({ lnd })
             .then((res) => res.chain_balance)
             .catch((e) => {
                 console.log(e)
             })
+        const info = await getWalletInfo({ lnd }).then((res) => res.public_key)
+        const peers = await getPeers({ lnd }).then((res) => res.peers)
         console.log('getChainBalance', chainBalance)
+        console.log('info', info)
+        console.log('peers', peers)
         return chainBalance
     },
 })
